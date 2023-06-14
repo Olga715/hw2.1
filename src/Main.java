315e9be4b9e2b16528aad8af9717a4fb49bc2552
Olga <olyab.715@gmail.com>
2023-06-14 18:49:29 +0500

    Gitignore added

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int amount = 16_015;
+        int mile_value = 20;
+        int miles = amount / mile_value;
+
+        System.out.println("Колличество начисленных миль: " + miles);
+
+    }
+}
